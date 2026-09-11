8ebf86d24b741f4f1a9a6d844112d0329147590e
benebeau214 <kimrn142@gmail.com>
2026-09-11 15:24:59 +0900

    검증 시나리오 완성

--- shop.py
+++ shop.py
@@ -33,3 +33,17 @@ class Order:
         amount = self.total_price()
         self.customer.add_points(amount)
         return amount
+
+
+c1 = Customer("김리나", "vip")
+c2 = Customer("박성현", "basic")
+
+o1 = Order("A-001", c1, [("딸기 마카롱", 3500), ("망고 스무디", 5000)])
+o1.pay()
+print(f"주문 총액 : {o1.total_price()}원, {c1.summary()}")
+o2 = Order("A-002", c2, [("바닐라 라떼", 4000), ("소금빵", 5500)])
+o2.pay()
+print(f"주문 총액 : {o2.total_price()}원, {c2.summary()}")
+o3 = Order("A-003", c1, [("그릭 요거트", 7000)])
+o3.pay()
+print(f"주문 총액 : {o3.total_price()}원, {c1.summary()}")
